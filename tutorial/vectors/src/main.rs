cb72d22470a27e2b4a2a9be3625a4ba708240dda
Shahtr1 <tramboos123@gmail.com>
2025-02-11 21:43:39 +0530

    Started Vectors

---
+++ tutorial/vectors/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let pizza_diameters = vec![8, 10, 12, 14];
+    // let pizza_diameters = Vec::<i32>::new();
+
+    println!("{pizza_diameters:?}");
+
+    // let pastas: Vec<&str> = Vec::new();
+    let pastas: Vec<&str> = vec!["Good", "Bad", "Worst"];
+    println!("{pastas:?}");
+}
