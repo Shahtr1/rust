6fe587fdaeb02140f52299fb639f118df2cb451c
Shahtr1 <tramboos123@gmail.com>
2025-02-24 14:40:14 +0530

    more on lifetimes

--- tutorial/lifetimes/src/main.rs
+++ tutorial/lifetimes/src/main.rs
@@ -1,14 +1,49 @@
+use std::char;
+
+pub fn capitalize(char_array: &mut [char]) {
+    for char in char_array.iter_mut() {
+        *char = char.to_uppercase().next().unwrap_or(*char);
+    }
+
+    println!("{:?}", char_array);
+}
+
 fn main() {
     let dog = String::from("Watson"); // referrant or lender
-    {
-        let my_pet = &dog;
-        println!("{my_pet}")
-    }
+    let my_pet = &dog;
+    println!("{my_pet}");
 
-    println!("{dog}");
+    let mut data = vec!['a', 'b', 'c'];
+    let slice = &mut data[..];
+    capitalize(slice);
 
-    {
-        let my_pet = &dog;
-        println!("{my_pet}")
-    }
+    data.push('d');
+    data.push('e');
+    data.push('f');
+
+    // println!("{:?}", slice); // will cause error above
+
+    let cities = vec![
+        String::from("London"),
+        String::from("New York"),
+        String::from("Barcelona"),
+    ];
+    let favourite_cities = &cities[0..2];
+
+    // drop(cities);
+
+    println!("{favourite_cities:?}");
+
+    /*
+    // This also wont work
+    let some_cities = {
+        let cities = vec![
+            String::from("London"),
+            String::from("New York"),
+            String::from("Barcelona"),
+        ];
+
+        &cities[0..2] // returns dangling reference
+    };
+    */
 }
