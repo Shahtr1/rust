dd42c800a688a41fd22799823c17a686ac0590d7
Shahtr1 <tramboos123@gmail.com>
2025-03-02 16:42:19 +0530

    Fn and FnMut

--- tutorial/closures/src/main.rs
+++ tutorial/closures/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let multiplier = 5;
+
+    let multiply_by = |value: i32| -> i32 { value * multiplier };
+
+    println!("{}", multiply_by(5));
+
+    let product = |a, b| a * b;
+    println!("{}", product(5 as u8, 2));
+    // println!("{}", product(1000, 2)); error as inferred type is u8
+
+    let mut numbers = vec![4, 8, 15, 16, 23, 42];
+    let print_numbers = || {
+        println!("{:?}", numbers);
+    };
+    print_numbers();
+    println!("{:?}", numbers);
+
+    let mut add_number = || numbers.push(100);
+    add_number();
+    add_number();
+
+    println!("{:?}", numbers);
 }
