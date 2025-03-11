8fa4940febb294cc7b3ce3527efa024a40d3eb6c
Shahtr1 <tramboos123@gmail.com>
2025-03-11 13:26:55 +0530

    added problems

--- tutorial/iterators/src/main.rs
+++ tutorial/iterators/src/main.rs
@@ -1,7 +1,20 @@
+use common::separator;
+use problem_1::{count_chars, count_words};
+
 mod manual;
+mod problem_1;
 mod traits;
 
 fn main() {
     manual::iteration();
     traits::iteration();
+    separator("Iteration problem");
+    println!(
+        "{:?}",
+        count_words("Sally sells sea shells by the sea shore")
+    );
+    println!(
+        "{:?}",
+        count_chars("Sally sells sea shells by the sea shore")
+    );
 }
