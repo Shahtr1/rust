a87a77fed3033f67701fd0ec4afa925159de22c0
Shahtr1 <tramboos123@gmail.com>
2025-03-12 14:28:47 +0530

    finished filter and find methods

--- tutorial/iterators/src/adapters.rs
+++ tutorial/iterators/src/adapters.rs
@@ -1,6 +1,20 @@
+use core::num;
 use std::collections::HashSet;
 
 use common::separator;
+
+#[derive(Debug, PartialEq, Eq)]
+enum ChannelType {
+    Comedy,
+    News,
+    ProgrammingTutorials,
+}
+
+#[derive(Debug)]
+struct TVChannel {
+    name: String,
+    channel_type: ChannelType,
+}
 
 pub fn run() {
     separator("Map Adapter");
@@ -48,4 +62,66 @@ pub fn run() {
     println!("{name_lengths:?}");
 
     separator("Filter and Find Adapters");
+
+    let numbers = [4, 8, 15, 16, 23, 42];
+
+    let evens: Vec<i32> = numbers
+        .into_iter()
+        .filter(|number| number % 2 == 0)
+        .collect();
+
+    let odds: Vec<i32> = numbers
+        .iter()
+        .filter(|number| *number % 2 != 0)
+        .copied()
+        .collect();
+
+    println!("{evens:?}");
+    println!("{odds:?}");
+
+    let first_even = numbers.into_iter().find(|number| number % 2 == 0);
+    println!("{first_even:?}");
+
+    let last_even = numbers.into_iter().rfind(|number| number % 2 == 0);
+    println!("{last_even:?}");
+
+    let channels = [
+        TVChannel {
+            name: String::from("CBS"),
+            channel_type: ChannelType::Comedy,
+        },
+        TVChannel {
+            name: String::from("RustLive"),
+            channel_type: ChannelType::ProgrammingTutorials,
+        },
+        TVChannel {
+            name: String::from("NBC"),
+            channel_type: ChannelType::News,
+        },
+        TVChannel {
+            name: String::from("RustTV"),
+            channel_type: ChannelType::ProgrammingTutorials,
+        },
+    ];
+
+    let good_channels: Vec<&TVChannel> = channels
+        .iter()
+        .filter(|channel| channel.channel_type == ChannelType::ProgrammingTutorials)
+        .collect();
+
+    println!("{good_channels:#?}");
+
+    let good_channels_names: Vec<String> = channels
+        .iter()
+        .filter(|channel| channel.channel_type == ChannelType::ProgrammingTutorials)
+        .map(|channel| channel.name.clone())
+        .collect();
+
+    println!("{good_channels_names:?}");
+
+    let good_channel = channels
+        .iter()
+        .find(|channel| channel.channel_type == ChannelType::ProgrammingTutorials);
+
+    println!("{good_channel:?}")
 }
